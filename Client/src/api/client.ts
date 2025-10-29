c2130ff72ee51c70cc6295fc0ec84ff5c92ac2ea
Adnan Ashraf <ashrafadnan959@gmail.com>
2025-10-29 23:35:58 +0530

    configured for deployment

--- Client/src/api/client.ts
+++ Client/src/api/client.ts
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://localhost:8000/api";
+const API_BASE_URL = "https://highway-delite-1hnq.vercel.app/api";
 
 export const apiClient = {
   get: async (endpoint: string) => {
